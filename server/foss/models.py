123401dd3129f690ca640e6de4c68ac70b2162ed
rahulbreezo <rahul@breezo.in>
2023-02-06 16:29:22 +0530

    Added account model

--- server/foss/models.py
+++ server/foss/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Account(models.Model):
+    auth_user = models.OneToOneField(
+        User, on_delete=models.CASCADE, null=False)
+    name = models.CharField(max_length=100, null=False)
+    email = models.EmailField(max_length=100, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
